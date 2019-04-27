ce1456ae7cd4ac8275a8692cad38989fb8102fa4
roadtovoid <donmezoa@mail.uc.edu>
2019-04-27 02:22:16 -0400

    Update download_reference_genome.sh

--- download_reference_genome.sh
+++ download_reference_genome.sh
@@ -12,5 +12,8 @@ efetch -db=nuccore -format=fasta -id=AF086833 > refs/reference.fa
 # Run BWA on downloaded data.
 bwa index refs/reference.fa
 
+# Index the reference genome for IGV.
+samtools faidx $REF
+
 # Generate perfect coverage.
 cat refs/reference.fa | python perfect_coverage.py
